a763caa3dcaa8b8272776bf90e4b9965e28d30ab
michaelpetty <michael.petty@gmail.com>
2020-05-18 12:37:18 -0700

    slack modal for instructors

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ const buildZoomPanes = zoomers => {
 const buildStudentProfile = zoomer => {
   const profileDiv = document.createElement('div');
   profileDiv.classList.add('window-main');
-  profileDiv.id = 'student-profile'
   profileDiv.insertAdjacentHTML('afterbegin', `
     <div class="student">
       <figure>
@@ -32,13 +31,56 @@ const buildStudentProfile = zoomer => {
   return profileDiv;
 }
 
+const buildInstructorProfile = (ele) => {
+  ele.querySelector('h3').innerText = '#sei-sf09-strictlybiz';
+  const profileDiv = document.createElement('div');
+  profileDiv.classList.add('window-main');
+  profileDiv.insertAdjacentHTML('afterbegin', `
+    <div class="instructor">
+      <div class="side">
+        <img src="./i/ga-slack.png" alt="slack channel" />
+      </div>
+      <div class="content">
+        <h4>Profile</h4>
+        <figure>
+          <img src="./i/profile/michael.png" alt="Michael Petty" />
+        </figure>
+        <h2>Michael Petty</h2>
+        <h5>SEI Instructor - SF</h5>
+        <ul>
+          <li>
+            <h6>LinkedIn</h6>
+            <a href="https://www.linkedin.com/in/michaelpetty42/">https://www.linkedin.com/in/michaelpetty42/</a>
+          </li>
+          <li>
+            <h6>GitHub</h6>
+            <a href="https://github.com/michaelpetty">https://github.com/michaelpetty</a>
+          </li>
+          <li>
+            <h6>Email Address</h6>
+            <a href="mailto:michael.petty@gmail.com">michael.petty@gmail.com</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `);
+  return profileDiv;
+
+}
+
 const displayZoom = (zoomers, ele) => {
   ele.insertAdjacentHTML('afterbegin', buildZoomPanes(zoomers).join(''));
 }
 
-const displayZoomModal = zoomer => {
-  document.getElementById('student-profile').replaceWith(buildStudentProfile(zoomer));
-  document.querySelector('.modal').classList.remove('hidden');
+const buildModal = (ele, type) => {
+  (type === 'zoom')? ele.classList.remove('slack') : ele.classList.remove('zoom');
+  ele.classList.add(type);
+}
+const displayZoomModal = (zoomer, ele) => {
+  buildModal(ele, 'zoom');
+  ele.querySelector('h3').innerText = `SEI 9 - ${zoomer[0]}`;
+  ele.querySelector('.window-main').replaceWith(buildStudentProfile(zoomer));
+  ele.classList.remove('hidden');
 }
 
 const randomizeListOrder = list => {
@@ -53,16 +95,20 @@ const randomizeListOrder = list => {
 
 const zoomersRandom = randomizeListOrder(userData);
 const zoomWindow = document.getElementById('zoom-classroom');
+const modalEle = document.querySelector('.modal');
 displayZoom(zoomersRandom, zoomWindow);
 
 zoomWindow.addEventListener('click', e => {
-  displayZoomModal(zoomersRandom[e.target.parentNode.parentNode.dataset.zoomerid]);
+  displayZoomModal(zoomersRandom[e.target.parentNode.parentNode.dataset.zoomerid], modalEle);
 })
 
 document.querySelector('.modal .buttons').addEventListener('click', e => {
-  document.querySelector('.modal').classList.add('hidden');
+  modalEle.classList.add('hidden');
 })
 
 document.querySelector('.ga-folder').addEventListener('click', e => {
-  console.log('clicked folder');
+  buildModal(modalEle, 'slack');
+  modalEle.querySelector('.window-main').replaceWith(buildInstructorProfile(modalEle));
+  //displayModal(modalEle);
+  modalEle.classList.remove('hidden');
 })
